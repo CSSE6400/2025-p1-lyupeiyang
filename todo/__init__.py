e388e638aa7f63dea99b890be8216f2660ee8350
lyupeiyang <1714087802@qq.com>
2025-02-28 20:24:39 +1000

    Week1 Practice

---
+++ todo/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+def create_app():
+    app=Flask(__name__)
+
+    from .views.routes import api
+    app.register_blueprint(api)
+
+    return app
+
+def main():
+    create_app()
+
+if __name__ == "__main__":
+    main()
